45cb94add8e654d8e7666c781a55c5e8039f02f4
Kaja Mikolajczyk <kajaths@gmail.com>
2018-10-08 20:57:57 +0000

    Fixed bug with redirecting to ticket creating page

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,5 @@ urlpatterns=[
      path('homepage', views.homepage, name='homepage'),
      path('project_info/', views.project_info, name="project_info"),
      path('our_promise/', views.promise, name="promise"),
-     path('stats/', views.show_stats, name="show_stats"),
+     # path('stats/', views.show_stats, name="show_stats"),
      ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from tickets.models import Ticket
-from chartit import PivotDataPool, PivotChart
 
 def homepage(request):
      
@@ -16,42 +15,42 @@ def promise(request):
     
     return render(request, 'promise.html')    
      
-def show_stats(request):
-# Step 1: Create a PivotDataPool with the data we want to retrieve.
-     ticketpivotdata = PivotDataPool(
-        series=[{
-            'options': {
-                'source': Ticket.objects.all(),
-                'categories': ['date_created'],
-                'legend_by': 'variety',
-                'top_n_per_cat': 3,
-            },
-            'terms': {
-                'upvotes': 'upvotes',
-            }
-        }]
-    )
+# def show_stats(request):
+# # Step 1: Create a PivotDataPool with the data we want to retrieve.
+#      ticketpivotdata = PivotDataPool(
+#         series=[{
+#             'options': {
+#                 'source': Ticket.objects.all(),
+#                 'categories': ['date_created'],
+#                 'legend_by': 'variety',
+#                 'top_n_per_cat': 3,
+#             },
+#             'terms': {
+#                 'upvotes': 'upvotes',
+#             }
+#         }]
+#     )
 
-    # Step 2: Create the PivotChart object
-     ticketcht = PivotChart(
-        datasource=ticketpivotdata,
-        series_options=[{
-            'options': {
-                'type': 'column',
-                'stacking': True
-            },
-            'terms': ['upvotes']
-        }],
-        chart_options={
-            'title': {
-                'text': 'Tickets_by_month'
-            },
-            'xAxis': {
-                'title': {
-                    'text': 'Month'
-                }
-            }
-        }
-    )
+#     # Step 2: Create the PivotChart object
+#      ticketcht = PivotChart(
+#         datasource=ticketpivotdata,
+#         series_options=[{
+#             'options': {
+#                 'type': 'column',
+#                 'stacking': True
+#             },
+#             'terms': ['upvotes']
+#         }],
+#         chart_options={
+#             'title': {
+#                 'text': 'Tickets_by_month'
+#             },
+#             'xAxis': {
+#                 'title': {
+#                     'text': 'Month'
+#                 }
+#             }
+#         }
+#     )
                        
-     return render(request, 'stats.html', {'ticketcht': ticketcht})
+#      return render(request, 'stats.html', {'ticketcht': ticketcht})
