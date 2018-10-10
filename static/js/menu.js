7e4019728c514af62bb7a2ec4699b8907142ac0c
Kaja Mikolajczyk <kajaths@gmail.com>
2018-10-10 17:27:01 +0000

    All is working as desired

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,11 +1,4 @@
 
-//      $(document).ready(function() {
-
-
-// 	 $('.sidenav').sidenav();
-   
-//   });
-
 
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
