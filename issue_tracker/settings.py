dfcf86be199b9b996b9cf66239683eddeeae9426
Kaja Mikolajczyk <kajaths@gmail.com>
2018-10-21 12:32:18 +0000

    Added test database and added python version in .travis.yml

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -87,18 +87,19 @@ WSGI_APPLICATION = 'issue_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 
-DATABASES = {
-    'default':  dj_database_url.parse(os.environ.get('DATABASE_URL')),
-}
-
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default':  dj_database_url.parse(os.environ.get('DATABASE_URL')),
+    }
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}   
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
