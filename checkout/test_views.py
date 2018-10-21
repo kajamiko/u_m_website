5288fb2dc3f926cd0d59276b863a43245ee9cb6a
Kaja Mikolajczyk <kajaths@gmail.com>
2018-10-21 12:46:52 +0000

    .

--- checkout/test_views.py
+++ checkout/test_views.py
@@ -4,4 +4,10 @@ from django.test import TestCase
 from django.conf import settings
 from tickets.models import Ticket
 from datetime import date
-from cart.models import Cart
+from cart.cart import Cart
+
+
+class TestCartViews(TestCase):
+     
+     def setUp(self):
+          self.client = Client()
